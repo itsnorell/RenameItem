e26254b7976878975f060dd500e4471363f58920
itsnorell <norell01q@gmail.com>
2025-03-03 23:33:24 +0300

    Update Main.java

--- src/main/java/norell/rename/Main.java
+++ src/main/java/norell/rename/Main.java
@@ -7,14 +7,12 @@ import norell.rename.forms.*;
 
 public class Main extends PluginBase {
 
-    public static int RenamePrice = 100;
-
     public final static int RenameItemFormID = 1;
 
     @Override
     public void onEnable() {
         this.getLogger().info(TextFormat.GREEN + "ReName [+]");
         getServer().getPluginManager().registerEvents(new RenameItemForm(), this);
-        getServer().getCommandMap().register("eisim", new RenameItemCommand());
+        getServer().getCommandMap().register("rename", new RenameItemCommand());
     }
 }
