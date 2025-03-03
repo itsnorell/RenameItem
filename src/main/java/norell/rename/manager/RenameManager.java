10cfb9aaa8d40b0fe1991cc9645a7319646e6bf1
itsnorell <norell01q@gmail.com>
2025-03-03 23:33:08 +0300

    Update RenameManager.java

--- src/main/java/norell/rename/manager/RenameManager.java
+++ src/main/java/norell/rename/manager/RenameManager.java
@@ -8,33 +8,31 @@ public class RenameManager {
 
     public static boolean renameItem(Player player, String input) {
         if (input == null || input.trim().isEmpty() || input.length() < 3 || input.length() > 15) {
-            player.sendMessage("§8» §cEşya adı 3 ile 15 karakter arasında olmalıdır.");
+            player.sendMessage("§8» §cItem name must be between 3 and 15 characters.");
             return false;
         }
 
         Item item = player.getInventory().getItemInHand();
         if (item.getId() == Item.AIR.getId()) {
-            player.sendMessage("§8» §cEline bir eşya al.");
+            player.sendMessage("§8» §cTake an item in your hand.");
             return false;
         }
 
         if (item.getCount() > 1) {
-            player.sendMessage("§8» §cTek seferde sadece 1 eşya isimlendirebilirsiniz.");
+            player.sendMessage("§8» §cYou can only name 1 item at a time.");
             return false;
         }
 
         if (!item.isArmor() && !item.isTool()) {
-            player.sendMessage("§8» §cElindeki eşya isimlendirilemez.");
+            player.sendMessage("§8» §cThe item cannot be renamed.");
             return false;
         }
 
-        int price = input.length() * Main.RenamePrice;
-        String name = "§e" + input + "\n§3İmzalayan: §b" + player.getName();
+        String name = "§e" + input + "\n§3Renamed by: §b" + player.getName();
         item.setCustomName(name);
         player.getInventory().setItemInHand(item);
 
-        player.sendMessage("§8» §aKesilen Ücret: §6" + price + " TL");
-        player.sendMessage("§8» §aEşya yeniden adlandırıldı.");
+        player.sendMessage("§8» §aItem renamed.");
         return true;
     }
 }
